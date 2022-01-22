2b4a4c534e70e8770f2d68d2d7e729f8c3be8414
stojkovmarjan <stojkovmarjan@gmail.com>
2022-01-22 20:41:38 +0100

    InitialCommit

--- app/src/main/java/com/example/dobcalc/MainActivity.kt
+++ app/src/main/java/com/example/dobcalc/MainActivity.kt
@@ -2,10 +2,42 @@ package com.example.dobcalc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import java.util.*;
+import android.app.DatePickerDialog;
+import java.text.SimpleDateFormat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnSelectDate = findViewById<Button>(R.id.btnSelectDate);
+
+        btnSelectDate.setOnClickListener{
+            clickDatePicker()
+        }
+
+    }
+
+    private fun clickDatePicker() {
+
+        val tvDate = findViewById<TextView>(R.id.tvDate);
+        val tvMinutes = findViewById<TextView>(R.id.tvMinutes);
+
+        val calendar = Calendar.getInstance();
+
+        val year = calendar.get(Calendar.YEAR);
+        val month = calendar.get(Calendar.MONTH);
+        val dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog( this, DatePickerDialog.OnDateSetListener{ _, y, m, d ->
+            var dateString = "$d/${m+1}/$y"
+            val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+            val date = sdf.parse(dateString)
+            tvDate.text = dateString
+        }, year, month,dayOfMonth).show();
+
     }
 }
